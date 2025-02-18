63e401409849ac6eac5245a2628618e216caa7bb
nguye2n <22024553@vnu.edu.vn>
2025-02-18 18:04:09 +0700

    spring boot crud example

---
+++ src/main/java/com/example/testing_001/Testing001Application.java
@@ -0,0 +1,14 @@
+package com.example.testing_001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+ // Đổi thành package chính xác của bạn
+public class Testing001Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Testing001Application.class, args);
+	}
+}
+
